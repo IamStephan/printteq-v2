423e83e97ef172259bc1c06b4afdfc6e177db47a
iamstephan <stephanburger54@gmail.com>
2021-08-01 20:47:14 +0200

    privacy policy page

---
+++ pages/privacy-policy.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import PrivacyPolicyHeader from "@sections/privacy_policy_header"
+
+const PrivacyPolicy = () => {
+  return (
+    <>
+      <PrivacyPolicyHeader />
+    </>
+  )
+}
+
+export default PrivacyPolicy
