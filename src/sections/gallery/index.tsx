ef884e9708a0a4625ca98e8c9961249086a2c078
iamstephan <stephanburger54@gmail.com>
2021-08-01 20:45:11 +0200

    gallery demo

--- src/sections/gallery/index.tsx
+++ src/sections/gallery/index.tsx
@@ -1,4 +1,12 @@
 import React from "react"
+import Img from "next/image"
+
+import Showcase01 from "@assets/sections/gallery/showcase_01.jpg"
+import Showcase02 from "@assets/sections/gallery/showcase_02.jpg"
+import Showcase03 from "@assets/sections/gallery/showcase_03.jpg"
+import Showcase04 from "@assets/sections/gallery/showcase_04.jpg"
+import Showcase05 from "@assets/sections/gallery/showcase_05.jpg"
+import Showcase06 from "@assets/sections/gallery/showcase_06.jpg"
 
 const Gallery = () => {
   return (
@@ -8,17 +16,19 @@ const Gallery = () => {
           <a href="/" aria-label="Item" className="mr-3">
             <div className="flex items-center justify-center w-16 h-16 rounded-full bg-red-50">
               <svg
-                className="w-12 h-12 text-red-600"
-                stroke="currentColor"
-                viewBox="0 0 52 52"
+                className="w-10 h-10 text-red-600"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <polygon
-                  strokeWidth="3"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  fill="none"
-                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M13 15.9C15.2822 15.4367 17 13.419 17 11V4H7V11C7 13.419 8.71776 15.4367 11 15.9V18H9V20H15V18H13V15.9ZM9 6H15V11C15 12.6569 13.6569 14 12 14C10.3431 14 9 12.6569 9 11V6Z"
+                  fill="currentColor"
                 />
+                <path d="M18 6H20V11H18V6Z" fill="currentColor" />
+                <path d="M6 6H4V11H6V6Z" fill="currentColor" />
               </svg>
             </div>
           </a>
@@ -28,50 +38,73 @@ const Gallery = () => {
           </h2>
         </div>
         <p className="w-full text-gray-700 lg:text-sm lg:max-w-md">
-          "Sed ut perspiciatis unde omnis iste natus error sit iste voluptatem
+          Sed ut perspiciatis unde omnis iste natus error sit iste voluptatem
           accusantium doloremque rem aperiam, ipsa eaque quae. Sed ut
-          perspiciatis unde omnis iste."
+          perspiciatis unde omnis iste.
         </p>
       </div>
       <div className="grid grid-cols-2 md:grid-cols-4 grid-rows-[repeat(4,auto)] md:grid-rows-[repeat(3,auto)] gap-6">
         <div className="relative overflow-hidden rounded-md aspect-w-1 aspect-h-1">
-          <img
-            src="https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+          <Img
+            src={Showcase01}
+            quality="100"
+            placeholder="blur"
+            layout="fill"
+            objectFit="cover"
             className="absolute inset-0 object-cover w-full h-full"
             alt=""
           />
         </div>
         <div className="relative overflow-hidden rounded-md aspect-w-1 aspect-h-1">
-          <img
-            src="https://images.pexels.com/photos/3182750/pexels-photo-3182750.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+          <Img
+            src={Showcase02}
+            quality="100"
+            placeholder="blur"
+            layout="fill"
+            objectFit="cover"
+            className="absolute inset-0 object-cover w-full h-full"
+            alt=""
+          />
+        </div>
+
+        <div className="relative col-span-2 overflow-hidden rounded-md aspect-w-3 aspect-h-2 sm:aspect-w-1 sm:aspect-h-1 sm:col-span-1 md:col-span-2 md:row-span-2 md:aspect-w-4 md:aspect-h-3">
+          <Img
+            src={Showcase04}
+            quality="100"
+            placeholder="blur"
+            layout="fill"
+            objectFit="cover"
             className="absolute inset-0 object-cover w-full h-full"
             alt=""
           />
         </div>
         <div className="relative col-span-2 overflow-hidden rounded-md aspect-w-3 aspect-h-2 sm:aspect-w-1 sm:aspect-h-1 sm:col-span-1 md:col-span-2 md:row-span-2 md:aspect-w-4 md:aspect-h-3">
-          <img
-            src="https://images.pexels.com/photos/3182746/pexels-photo-3182746.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-            className="absolute inset-0 object-cover w-full h-full"
-            alt=""
-          />
-        </div>
-        <div className="relative col-span-2 overflow-hidden rounded-md aspect-w-3 aspect-h-2 sm:aspect-w-1 sm:aspect-h-1 sm:col-span-1 md:col-span-2 md:row-span-2 md:aspect-w-4 md:aspect-h-3">
-          <img
-            src="https://images.pexels.com/photos/3184296/pexels-photo-3184296.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-            className="absolute inset-0 object-cover w-full h-full"
-            alt=""
-          />
-        </div>
-        <div className="relative overflow-hidden rounded-md aspect-w-1 aspect-h-1">
-          <img
-            src="https://images.pexels.com/photos/3184311/pexels-photo-3184311.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+          <Img
+            src={Showcase03}
+            quality="100"
+            placeholder="blur"
+            layout="fill"
+            objectFit="cover"
             className="absolute inset-0 object-cover w-full h-full"
             alt=""
           />
         </div>
         <div className="relative overflow-hidden rounded-md aspect-w-1 aspect-h-1">
-          <img
-            src="https://images.pexels.com/photos/3184338/pexels-photo-3184338.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+          <Img
+            src={Showcase05}
+            placeholder="blur"
+            layout="fill"
+            objectFit="cover"
+            className="absolute inset-0 object-cover w-full h-full"
+            alt=""
+          />
+        </div>
+        <div className="relative overflow-hidden rounded-md aspect-w-1 aspect-h-1">
+          <Img
+            src={Showcase06}
+            placeholder="blur"
+            layout="fill"
+            objectFit="cover"
             className="absolute inset-0 object-cover w-full h-full"
             alt=""
           />
