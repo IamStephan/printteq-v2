1d66035560797f9765f1bfb6e5a5f57b1eb6d83a
iamstephan <stephanburger54@gmail.com>
2021-08-13 21:02:33 +0200

    include Body (for later use)

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 
 import PrivacyPolicyHeader from "@sections/privacy_policy_header"
+import PrivacyPolicyBody from "@sections/privacy_policy_body"
 
 const PrivacyPolicy = () => {
   return (
     <>
       <PrivacyPolicyHeader />
+      <PrivacyPolicyBody />
     </>
   )
 }
