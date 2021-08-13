051b573dcc6193a11ab7411262745894261b9753
iamstephan <stephanburger54@gmail.com>
2021-08-13 21:01:54 +0200

    policy body (for later use

---
+++ src/sections/privacy_policy_body/index.tsx
@@ -0,0 +1,176 @@
+import React from "react"
+
+// TODO: Provide relevant information
+const PrivacyPolicyBody = () => {
+  return (
+    <section className="px-4 py-16 mx-auto prose sm:max-w-full lg:max-w-screen-lg md:px-24 lg:px-8 lg:py-20">
+      <h1>
+        <strong>THE PROTECTION OF PERSONAL INFORMATION ACT</strong>
+      </h1>
+      <h2>
+        <strong>CUSTOMER PRIVACY NOTICE</strong>
+      </h2>
+      <p>
+        This Notice explains how we obtain, use and disclose your personal
+        information, in accordance with the requirements of the Protection of
+        Personal Information Act (“POPIA”).
+      </p>
+      <p>
+        At Printteq (and including this website, POPI Act-Compliance) we are
+        committed to protecting your privacy and to ensure that your personal
+        information is collected and used properly, lawfully and transparently.
+      </p>
+      <h3>
+        <strong>About the Company</strong>
+      </h3>
+      <p>Kogelpark Kliniek BK T/A Printteq</p>
+      <h3>
+        <strong>The information we collect </strong>
+      </h3>
+      <p>
+        We collect and process your personal information mainly to contact you
+        for the purposes of understanding your requirements, and delivering
+        services accordingly. &nbsp;For this purpose we will collect contact
+        details including your name and organisation.
+      </p>
+      <p>
+        We collect information directly from you where you provide us with your
+        personal details.&nbsp; Where possible, we will inform you what
+        information you are required to provide to us and what information is
+        optional.
+      </p>
+      <p>
+        Website usage information may be collected using “cookies” which allows
+        us to collect standard internet visitor usage information.
+      </p>
+
+      <h3>
+        <strong>How we use your information </strong>
+      </h3>
+      <p>
+        We will use your personal information only for the purposes for which it
+        was collected and agreed with you.&nbsp; In addition, where necessary
+        your information may be retained for legal or research purposes.&nbsp;
+      </p>
+      <p>For example:</p>
+      <ul>
+        <li>To gather contact information;</li>
+        <li>
+          To confirm and verify your identity or to verify that you are an
+          authorised user for security purposes;
+        </li>
+        <li>
+          For the detection and prevention of fraud, crime, money laundering or
+          other malpractice;
+        </li>
+        <li>
+          To conduct market or customer satisfaction research or for statistical
+          analysis;
+        </li>
+        <li>For audit and record keeping purposes;</li>
+        <li>In connection with legal proceedings.</li>
+      </ul>
+      <h3>
+        <strong>Disclosure of information </strong>
+      </h3>
+      <p>
+        We may disclose your personal information to our service providers who
+        are involved in the delivery of products or services to you. We have
+        agreements in place to ensure that they comply with the privacy
+        requirements as required by the Protection of Personal Information Act.
+      </p>
+      <p>We may also disclose your information:</p>
+      <ul>
+        <li>
+          Where we have a duty or a right to disclose in terms of law or
+          industry codes;
+        </li>
+        <li>Where we believe it is necessary to protect our rights.</li>
+      </ul>
+      <h3>
+        <strong>Information Security </strong>
+      </h3>
+      <p>
+        We are legally obliged to provide adequate protection for the personal
+        information we hold and to stop unauthorized access and use of personal
+        information. We will, on an on-going basis, continue to review our
+        security controls and related processes to ensure that your personal
+        information remains secure.
+      </p>
+      <p>Our security policies and procedures cover:</p>
+      <ul>
+        <li>Physical security;</li>
+        <li>Computer and network security;</li>
+        <li>Access to personal information;</li>
+        <li>Secure communications;</li>
+        <li>Security in contracting out activities or functions;</li>
+        <li>Retention and disposal of information;</li>
+        <li>Acceptable usage of personal information;</li>
+        <li>Governance and regulatory issues;</li>
+        <li>Monitoring access and usage of private information;</li>
+        <li>Investigating and reacting to security incidents.</li>
+      </ul>
+      <p>
+        When we contract with third parties, we impose appropriate security,
+        privacy and confidentiality obligations on them to ensure that personal
+        information that we remain responsible for, is kept secure.
+      </p>
+      <p>
+        We will ensure that anyone to whom we pass your personal information
+        agrees to treat your information with the same level of protection as we
+        are obliged to.
+      </p>
+
+      <h3>
+        <strong>Your Rights: Access to information </strong>
+      </h3>
+      <p>
+        You have the right to request a copy of the personal information we hold
+        about you. To do this, simply contact us at the numbers/addresses as
+        provided on our website and specify what information you require.&nbsp;
+        We will need a copy of your ID document to confirm your identity before
+        providing details of your personal information.
+      </p>
+      <p>
+        Please note that any such access request may be subject to a payment of
+        a legally allowable fee.&nbsp; &nbsp;
+      </p>
+
+      <h3>
+        <strong>Correction of your information </strong>
+      </h3>
+      <p>
+        You have the right to ask us to update, correct or delete your personal
+        information. We will require a copy of your ID document to confirm your
+        identity before making changes to personal information we may hold about
+        you. We would appreciate it if you would keep your personal information
+        accurate.
+      </p>
+
+      <h3>
+        <strong>Definition of personal information </strong>
+      </h3>
+      <p>
+        According to the Act <strong>‘‘personal information’’ </strong>means
+        information relating to an identifiable, living, natural person, and
+        where it is applicable, an identifiable, existing juristic person.
+        &nbsp;Further to the POPI Act, Printteq also includes the following
+        items as personal information:
+      </p>
+      <ul>
+        <li>Email addresses.</li>
+      </ul>
+      <h3>
+        <strong>How to contact us </strong>
+      </h3>
+      <p>
+        If you have any queries about this notice; you need further information
+        about our privacy practices; wish to withdraw consent; exercise
+        preferences or access or correct your personal information, please
+        contact us at the numbers/addresses listed on our website.
+      </p>
+    </section>
+  )
+}
+
+export default PrivacyPolicyBody
