1882640059cef9c1aa95c22b0ef182cbc099d00b
iamstephan <stephanburger54@gmail.com>
2021-08-13 21:03:30 +0200

    test Hash links and new text

--- src/sections/benefits/index.tsx
+++ src/sections/benefits/index.tsx
@@ -5,7 +5,10 @@ import SignageImg from "@assets/sections/services/signage.jpg"
 
 const Benefits = () => {
   return (
-    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div
+      id="benefits"
+      className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"
+    >
       <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
         <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
           The quick, brown fox jumps over a lazy dog
@@ -30,7 +33,9 @@ const Benefits = () => {
               />
             </svg>
           </div>
-          <h6 className="mb-3 text-xl font-bold leading-5">A business big</h6>
+          <h6 className="mb-3 text-xl font-bold leading-5">
+            Stand-out among the crowd
+          </h6>
           <p className="mb-6 text-sm text-gray-900">
             Lookout flogging bilge rat main sheet bilge water nipper fluke to go
             on account heave down clap of thunder.
