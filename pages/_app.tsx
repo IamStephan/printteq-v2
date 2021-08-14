9cb12c9861a5f0a9bb559dd3432b556d7bf90259
iamstephan <stephanburger54@gmail.com>
2021-08-14 16:34:02 +0200

    using new scmooth scroll

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import type { AppProps } from "next/app"
 
+import { useNormalScrollRoutes } from "@hooks/useNormalScrollRoutes"
 import Layout from "@templates/layout"
+
 import "../styles/globals.css"
 
 const _app: React.FC<AppProps> = ({ Component, pageProps }) => {
+  useNormalScrollRoutes()
+
   return (
     <Layout>
       <Component {...pageProps} />
