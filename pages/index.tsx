47083a16eb5d90b4e3493b138470ae47adeb906f
iamstephan <stephanburger54@gmail.com>
2021-08-17 22:31:09 +0200

    added local business json-ld

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { LocalBusinessJsonLd } from "next-seo"
 
 import Hero from "@sections/hero"
 import SocialProof from "@sections/social_proof"
@@ -11,6 +12,25 @@ import CTA from "@sections/cta"
 const Home: React.FC = () => {
   return (
     <>
+      <LocalBusinessJsonLd
+        type="Store"
+        id="https://printteq.co.za"
+        name="Printteq | We Print It All"
+        description="Digital Printing, Signage & Engraving Solutions."
+        telephone="022 065 0067"
+        address={{
+          streetAddress: "Unit 7, 844@Oostewal Centre, Oostewal Road",
+          addressCountry: "ZA",
+          postalCode: "7357",
+          addressLocality: "Langebaan, Western Cape",
+        }}
+        sameAs={["https://facebook.com/PRINTTEQ"]}
+        images={[
+          "https://printteq.co.za/preview_lg.jpg",
+          "https://printteq.co.za/preview_md.jpg",
+          "https://printteq.co.za/preview_sm.png",
+        ]}
+      />
       <Hero />
       <SocialProof />
       <Benefits />
