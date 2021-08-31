3aebf08465c459ecd23d7c7ae5e52602bb69f16b
iamstephan <stephanburger54@gmail.com>
2021-08-31 22:55:31 +0200

    added image domains

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ["scontent.cdninstagram.com", "video.cdninstagram.com"],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
