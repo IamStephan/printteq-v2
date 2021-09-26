b2d34d9ef46cd4e13ae5856a951728a767345fdc
iamstephan <stephanburger54@gmail.com>
2021-09-26 22:41:29 +0200

    removed new pill

--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -37,9 +37,6 @@ const Hero = () => {
       </div>
       <div className="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
         <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
-          <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-red-600 uppercase rounded-full bg-red-50 ring-1 ring-red-600">
-            Brand new
-          </p>
           <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-white lg:text-gray-900 sm:text-5xl sm:leading-none">
             Digital Printing & <br className="hidden md:block" />
             Engraving{" "}
