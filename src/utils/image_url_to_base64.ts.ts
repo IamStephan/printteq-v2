aac132fb864821ead90973dd45a3dd8c208c6f83
iamstephan <stephanburger54@gmail.com>
2021-09-26 22:37:20 +0200

    base 64 encode util

---
+++ src/utils/image_url_to_base64.ts.ts
@@ -0,0 +1,6 @@
+export const ImageUrlToBase64 = async (url: string, type: string) => {
+  const res = await fetch(url)
+  const arrayBuff = await res.arrayBuffer()
+
+  return `data:${type};base64,${Buffer.from(arrayBuff).toString("base64url")}`
+}
